c0e7b340b678e5f4f8a232853e422f44d67cbe81
lemonlxn <38677657+lemonlxn@users.noreply.github.com>
2019-04-20 23:01:56 +0800

    在继承案例中，增加Dog类

--- src/lxn/lemon/animal/Dog.java
+++ src/lxn/lemon/animal/Dog.java
@@ -5,14 +5,28 @@ package lxn.lemon.animal;
  * Time :  2019-04-20 16:22
  */
 public class Dog extends Animal {
+    private String species;
+
     public Dog(){
 
     }
 
+    public Dog(String name,int month,String species){
+        super(name, month);
+        this.setSpecies(species);
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
     @Override
     public void sleep(String name){
-
-
+        System.out.println(this.getName() + " is sleeping");
     }
 
 }
